82ba48105761471c58a8c547ac72f591e474c679
tllanka <tllankaweb@gmail.com>
2026-04-19 10:46:06 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const VER='spice-pos-v3';
+const CACHE=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(VER).then(c=>c.addAll(CACHE)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==VER).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||!e.request.url.startsWith('http'))return;e.respondWith(caches.match(e.request).then(cached=>{if(cached)return cached;return fetch(e.request).then(res=>{if(!res||res.status!==200||res.type==='opaque')return res;const clone=res.clone();caches.open(VER).then(c=>c.put(e.request,clone));return res;}).catch(()=>caches.match('./index.html'));}));});
