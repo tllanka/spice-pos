55db6d5780b6d2effc2c2095ac60c553ea3a02a3
tllanka <tllankaweb@gmail.com>
2026-04-21 07:37:00 +0530

    Update service worker version to spice-pos-v35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VER='spice-pos-v34';
+const VER='spice-pos-v35';
 const CACHE=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(VER).then(c=>c.addAll(CACHE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==VER).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
